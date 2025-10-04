06c10aed918b2d50d2272d32ce6f4c3b0b359576
jareth182 <jarethtrujilloticse@gmail.com>
2025-10-04 15:20:26 -0500

    Proyecto inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Examen_parcial_2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
